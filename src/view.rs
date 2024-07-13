6a16b9c9cf0fa6df37aa02162c508ac78cfc5aca
unknown <mgtlake@outlook.com>
2024-07-13 15:59:41 +1000

    Edit all stats

--- src/view.rs
+++ src/view.rs
@@ -42,9 +42,9 @@ enum Message {
     UpdateStrengthBase(u32),
     UpdateAccuracy(u32),
     UpdateAccuracyBase(u32),
-    UpdateRelfexes(u32),
-    UpdateReflexBase(u32),
-    UpdateBraveryUnits(u32),
+    UpdateReflexes(u32),
+    UpdateReflexesBase(u32),
+    UpdateBravery(u32),
     UpdateBraveryBase(u32),
 }
 
@@ -60,83 +60,98 @@ impl Sandbox for Editor {
     }
 
     fn update(&mut self, message: Message) {
-        match message {
-            Message::OpenFile => {
-                let path = FileDialog::new()
-                    .add_filter("Save file", &["sav"])
-                    .pick_file();
+        if let Message::OpenFile = message {
+            let path = FileDialog::new()
+                .add_filter("Save file", &["sav"])
+                .pick_file();
 
-                if let Some(path) = path {
-                    let save_or_error = load_save(&path);
-                    *self = match save_or_error {
-                        Ok(save) => {
-                            let selected_soldier_id =
-                                save.soldiers.get(0).map(|soldier| soldier.id).unwrap_or(0);
-                            Editor::Save {
-                                path,
-                                save,
-                                selected_soldier_id,
-                            }
+            if let Some(path) = path {
+                let save_or_error = load_save(&path);
+                *self = match save_or_error {
+                    Ok(save) => {
+                        let selected_soldier_id =
+                            save.soldiers.get(0).map(|soldier| soldier.id).unwrap_or(0);
+                        Editor::Save {
+                            path,
+                            save,
+                            selected_soldier_id,
                         }
-                        Err(e) => {
-                            MessageDialog::new()
-                                .set_level(MessageLevel::Error)
-                                .set_title("Could not open save file!")
-                                .set_description(format!("{:#?}", e))
-                                .show();
-                            Editor::NoData
-                        }
-                    };
-                }
-            }
-            Message::SaveFile => {
-                if let Editor::Save { path, save, .. } = &self {
-                    if let Err(e) = fs::write(path, save.serialise()) {
+                    }
+                    Err(e) => {
                         MessageDialog::new()
                             .set_level(MessageLevel::Error)
-                            .set_title("Could not write updated save file!")
+                            .set_title("Could not open save file!")
                             .set_description(format!("{:#?}", e))
                             .show();
+                        Editor::NoData
                     }
+                };
+            }
+        }
+
+        if let Editor::Save {
+            path,
+            save,
+            selected_soldier_id,
+        } = self
+        {
+            if let Message::SaveFile = message {
+                if let Err(e) = fs::write(path, save.serialise()) {
+                    MessageDialog::new()
+                        .set_level(MessageLevel::Error)
+                        .set_title("Could not write updated save file!")
+                        .set_description(format!("{:#?}", e))
+                        .show();
                 }
             }
-            Message::SelectSoldier { id } => {
-                if let Editor::Save {
-                    selected_soldier_id,
-                    ..
-                } = self
-                {
-                    *selected_soldier_id = id as u32;
-                }
+            if let Message::SelectSoldier { id } = message {
+                *selected_soldier_id = id as u32;
             }
-            Message::GenderSelected(gender) => {
-                if let Editor::Save {
-                    selected_soldier_id,
-                    save,
-                    ..
-                } = self
-                {
-                    if let Some(soldier) = save.get_soldier_mut(*selected_soldier_id) {
+
+            if let Some(soldier) = save.get_soldier_mut(*selected_soldier_id) {
+                match message {
+                    Message::GenderSelected(gender) => {
                         soldier.gender = gender;
                     }
-                }
-            }
-            Message::UpdateTimeUnits(val) => {
-                if let Editor::Save {
-                    selected_soldier_id,
-                    save,
-                    ..
-                } = self
-                {
-                    if let Some(soldier) = save.get_soldier_mut(*selected_soldier_id) {
+                    Message::UpdateTimeUnits(val) => {
                         if val < soldier.stats.time_units_original {
                             return;
                         }
                         soldier.stats.time_units_current = val;
                     }
+                    Message::UpdateHealth(val) => {
+                        if val < soldier.stats.health_original {
+                            return;
+                        }
+                        soldier.stats.health_current = val;
+                    }
+                    Message::UpdateStrength(val) => {
+                        if val < soldier.stats.strength_original {
+                            return;
+                        }
+                        soldier.stats.strength_current = val;
+                    }
+                    Message::UpdateAccuracy(val) => {
+                        if val < soldier.stats.accuracy_original {
+                            return;
+                        }
+                        soldier.stats.accuracy_current = val;
+                    }
+                    Message::UpdateReflexes(val) => {
+                        if val < soldier.stats.reflexes_original {
+                            return;
+                        }
+                        soldier.stats.reflexes_current = val;
+                    }
+                    Message::UpdateBravery(val) => {
+                        if val < soldier.stats.bravery_original {
+                            return;
+                        }
+                        soldier.stats.bravery_current = val;
+                    }
+                    _ => {}
                 }
             }
-            _ => {}
         }
     }
 
@@ -246,46 +261,84 @@ fn view_soldier_editor(soldier: &Soldier) -> Element<Message> {
                 text_input("Soldier nationality", soldier.nationality.as_str()),
             ],
         ]
-        .spacing(10)
-        .padding(10)],
+        .spacing(10)],
         view_soldier_stats_editor(&soldier.stats),
     ]
+    .spacing(20)
+    .padding(10)
     .into()
 }
 
 fn view_soldier_stats_editor(stats: &SoldierStats) -> Element<Message> {
-    let rows = [(
-        "Time units",
-        stats.time_units_current,
-        Message::UpdateTimeUnits,
-        stats.time_units_original,
-        Message::UpdateTimeUnitsBase,
-    )];
+    column![
+        view_soldier_stats_editor_row(
+            "Time units",
+            stats.time_units_current,
+            Message::UpdateTimeUnits,
+            stats.time_units_original,
+            Message::UpdateTimeUnitsBase,
+        ),
+        view_soldier_stats_editor_row(
+            "Health",
+            stats.health_current,
+            Message::UpdateHealth,
+            stats.health_original,
+            Message::UpdateHealthBase,
+        ),
+        view_soldier_stats_editor_row(
+            "Strength",
+            stats.strength_current,
+            Message::UpdateStrength,
+            stats.strength_original,
+            Message::UpdateStrengthBase,
+        ),
+        view_soldier_stats_editor_row(
+            "Accuracy",
+            stats.accuracy_current,
+            Message::UpdateAccuracy,
+            stats.accuracy_original,
+            Message::UpdateAccuracyBase,
+        ),
+        view_soldier_stats_editor_row(
+            "Reflexes",
+            stats.reflexes_current,
+            Message::UpdateReflexes,
+            stats.accuracy_original,
+            Message::UpdateReflexesBase,
+        ),
+        view_soldier_stats_editor_row(
+            "Bravery",
+            stats.bravery_current,
+            Message::UpdateBravery,
+            stats.bravery_original,
+            Message::UpdateBraveryBase,
+        ),
+    ]
+    .spacing(10)
+    .into()
+}
 
-    column(
-        // TODO checks bounds in game
-        rows.into_iter()
-            .map(
-                |(stat_name, current, update_current, original, update_original)| {
-                    row![
-                        text(stat_name).size(20),
-                        horizontal_space().width(Length::Fixed(10.0)),
-                        text(current)
-                            .size(20)
-                            .width(Length::Fixed(30.0))
-                            .horizontal_alignment(Horizontal::Center),
-                        horizontal_space().width(Length::Fixed(10.0)),
-                        slider(1..=100, current, update_current),
-                        horizontal_space().width(Length::Fixed(20.0)),
-                        text("Base value").size(20),
-                        horizontal_space().width(Length::Fixed(10.0)),
-                        text_input("Stat base value", &original.to_string()),
-                    ]
-                    .into()
-                },
-            )
-            .collect::<Vec<_>>(),
-    )
+fn view_soldier_stats_editor_row(
+    stat_name: &str,
+    current: u32,
+    update_current: fn(u32) -> Message,
+    base: u32,
+    update_base: fn(u32) -> Message,
+) -> Element<Message> {
+    row![
+        text(stat_name).size(20),
+        horizontal_space().width(Length::Fixed(10.0)),
+        text(current)
+            .size(20)
+            .width(Length::Fixed(30.0))
+            .horizontal_alignment(Horizontal::Center),
+        horizontal_space().width(Length::Fixed(10.0)),
+        slider(1..=100, current, update_current),
+        horizontal_space().width(Length::Fixed(20.0)),
+        text("Base value").size(20),
+        horizontal_space().width(Length::Fixed(10.0)),
+        text_input("Stat base value", &base.to_string()),
+    ]
     .into()
 }
 
